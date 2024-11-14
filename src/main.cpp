8febf6600d8bd864ef42b17d174bac7aa7fbb227
Naxrin <94737247+Naxrin@users.noreply.github.com>
2024-11-14 23:26:30 +0800

    ★加一个按键
    曲目详情界面SongInfoLayer加一个按键redirect到代理的网页

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/modify/CCHttpClient.hpp>
+#include <Geode/modify/SongInfoLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -29,5 +30,33 @@ class $modify(_, CCHttpClient) {
 	    req->setUrl(final_url);
 
         CCHttpClient::send(req);
+    }
+};
+
+class $modify(MySongInfoLayer, SongInfoLayer) {
+    bool init(gd::string p0, gd::string p1, gd::string p2, gd::string p3, gd::string p4, gd::string p5, int p6, gd::string p7, int p8) {
+        if (!SongInfoLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8))
+            return false;
+        
+        auto layer = static_cast<CCNode*>(getChildren()->objectAtIndex(0));
+        auto menu = static_cast<CCMenu*>(layer->getChildren()->objectAtIndex(3));
+
+        auto dldBtn = static_cast<CCNode*>(menu->getChildren()->objectAtIndex(2));
+        auto pos = dldBtn->getPosition();
+        dldBtn->setPosition(CCPoint(pos.x, pos.y+10.f));
+
+        auto proxySpr = ButtonSprite::create("Download from Endless Proxy", "bigFont.fnt", "GJ_button_01.png");
+        proxySpr->setScale(0.6);
+        auto proxyBtn = CCMenuItemSpriteExtra::create(proxySpr, this, menu_selector(MySongInfoLayer::onProxyWebPage));
+        proxyBtn->setPosition(CCPoint(pos.x, pos.y-30.f));
+
+        menu->addChild(proxyBtn);
+        return true;
+    }
+
+    void onProxyWebPage(CCObject*) {
+
+        std::string link = "start https://endless-services.zhazha120.cn/endless_proxy/newgrounds/audios/" + std::to_string(m_songID) + ".mp3";
+        system(link.c_str());
     }
 };
